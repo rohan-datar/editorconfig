020f8547ecd36bdeb43442a17b4f9862f1daebb6
Rohan Datar <me@rohandatar.com>
2025-12-19 00:15:30 -0600

    basic emacs config

--- emacs/default.nix
+++ emacs/default.nix
@@ -1,64 +1,65 @@
 {
-
   perSystem =
     {
       pkgs,
       config,
       ...
     }:
-    {
-      packages.emacs = pkgs.emacsWithPackagesFromUsePackage {
-        # Your Emacs config file. Org mode babel files are also
-        # supported.
-        # NB: Config files cannot contain unicode characters, since
-        #     they're being parsed in nix, which lacks unicode
-        #     support.
-        # config = ./emacs.org;
-        config = ./emacs.el;
-
-        # Whether to include your config as a default init file.
-        # If being bool, the value of config is used.
-        # Its value can also be a derivation like this if you want to do some
-        # substitution:
-        #   defaultInitFile = pkgs.substituteAll {
-        #     name = "default.el";
-        #     src = ./emacs.el;
-        #     inherit (config.xdg) configHome dataHome;
-        #   };
-        defaultInitFile = true;
-
-        # By default emacsWithPackagesFromUsePackage will only pull in
-        # packages with `:ensure`, `:ensure t` or `:ensure <package name>`.
-        # Setting `alwaysEnsure` to `true` emulates `use-package-always-ensure`
-        # and pulls in all use-package references not explicitly disabled via
-        # `:ensure nil` or `:disabled`.
-        # Note that this is NOT recommended unless you've actually set
-        # `use-package-always-ensure` to `t` in your config.
+    let
+      # Use emacs-pgtk for native Wayland support
+      rdmacs = pkgs.emacsWithPackagesFromUsePackage {
+        package = pkgs.emacs-pgtk;
+        config = ./emacs.org;
+        defaultInitFile = false;
         alwaysEnsure = true;
-
-        # For Org mode babel files, by default only code blocks with
-        # `:tangle yes` are considered. Setting `alwaysTangle` to `true`
-        # will include all code blocks missing the `:tangle` argument,
-        # defaulting it to `yes`.
-        # Note that this is NOT recommended unless you have something like
-        # `#+PROPERTY: header-args:emacs-lisp :tangle yes` in your config,
-        # which defaults `:tangle` to `yes`.
         alwaysTangle = true;
-
-        # Optionally provide extra packages not in the configuration file.
-        # This can also include extra executables to be run by Emacs (linters,
-        # language servers, formatters, etc)
         extraEmacsPackages = epkgs: [
           epkgs.cask
           pkgs.shellcheck
+          pkgs.rust-analyzer
+          pkgs.nil
         ];
+      };
 
-        # Optionally override derivations.
-        # override = final: prev: {
-        #   weechat = prev.melpaPackages.weechat.overrideAttrs (old: {
-        #     patches = [ ./weechat-el.patch ];
-        #   });
-        # };
+      # Store the config path
+      emacsConfig = ./emacs.org;
+
+      # Wrapper script that loads config from the source directory
+      rdmacs-test = pkgs.writeShellScriptBin "rdmacs-test" ''
+        # Use a separate user-emacs-directory to avoid conflicts
+        export HOME="''${XDG_RUNTIME_DIR:-/tmp}/rdmacs-test-$$"
+        mkdir -p "$HOME/.emacs.d"
+
+        # Create a minimal init that loads the org config
+        # We specify a writable output path for the tangled file
+        cat > "$HOME/.emacs.d/init.el" << EOF
+        (require 'org)
+        (defvar rdmacs-config-org "${emacsConfig}")
+        (defvar rdmacs-config-el "$HOME/.emacs.d/config.el")
+        (org-babel-tangle-file rdmacs-config-org rdmacs-config-el)
+        (load-file rdmacs-config-el)
+        EOF
+
+        exec ${rdmacs}/bin/emacs "$@"
+      '';
+    in
+    {
+      packages = {
+        rdmacs = rdmacs;
+        rdmacs-test = rdmacs-test;
+      };
+
+      apps = {
+        rdmacs = {
+          type = "app";
+          program = "${rdmacs}/bin/emacs";
+          meta.description = "Emacs with packages from use-package config";
+        };
+        rdmacs-test = {
+          type = "app";
+          program = "${rdmacs-test}/bin/rdmacs-test";
+          meta.description = "Emacs with live-editable config from source directory";
+        };
       };
     };
 }
