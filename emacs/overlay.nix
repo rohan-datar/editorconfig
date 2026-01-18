be1468ee2aa162f4d8689e674f9ba59e5288eed8
Rohan Datar <me@rohandatar.com>
2026-01-18 15:36:55 -0600

    Add support for package deps to overlay

--- emacs/overlay.nix
+++ emacs/overlay.nix
@@ -2,6 +2,23 @@
 # Similar to NixCats' standardPluginOverlay for neovimPlugins
 inputs: final: prev:
 let
+  inherit (final) emacs-pgtk;
+  epkgs = emacs-pgtk.pkgs;
+
+  # Define dependencies for packages that need them
+  # This is similar to how you'd specify dependencies in a MELPA recipe
+  packageDeps = {
+    # Add packages that need dependencies here
+    # doom-dashboard = with epkgs; [
+    #   dashboard
+    #   nerd-icons
+    # ];
+  };
+
+  # Packages that should skip byte-compilation
+  skipCompilation = [
+  ];
+
   # Filter inputs that start with "emacs-" and have flake = false
   emacsInputs = builtins.filter (
     name: builtins.match "emacs-.*" name != null && inputs.${name} ? outPath
@@ -13,14 +30,28 @@ let
     let
       # Remove "emacs-" prefix to get the package name
       pkgName = builtins.substring 6 (builtins.stringLength name) name;
+      shouldSkipCompile = builtins.elem pkgName skipCompilation;
     in
     {
       name = pkgName;
-      value = final.emacs-pgtk.pkgs.trivialBuild {
-        pname = pkgName;
-        version = "unstable-${inputs.${name}.lastModifiedDate or "unknown"}";
-        src = inputs.${name};
-      };
+      value =
+        final.emacs-pgtk.pkgs.trivialBuild {
+          pname = pkgName;
+          version = "unstable-${inputs.${name}.lastModifiedDate or "unknown"}";
+          src = inputs.${name};
+          packageRequires = packageDeps.${pkgName} or [ ];
+        }
+        // (
+          if shouldSkipCompile then
+            {
+              buildPhase = ''
+                  runHook preBuild
+                runHook postBuild
+              '';
+            }
+          else
+            { }
+        );
     };
 
   # Build all Emacs packages from inputs
