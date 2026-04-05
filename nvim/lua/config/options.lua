fd33c0d9552178b6d821ad240201efdbb7dd6aae
Rohan Datar <me@rohandatar.com>
2026-04-05 00:41:59 -0500

    enable nvim ui2

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -67,3 +67,52 @@ vim.opt.undofile = true
 
 -- For Obsidian plugin, remove if it causes issues
 vim.opt.conceallevel = 1
+
+-- Experimental UI2: floating cmdline and messages
+o.cmdheight = 0
+require("vim._core.ui2").enable({
+	enable = true,
+	msg = {
+		targets = {
+			[""] = "msg",
+			empty = "cmd",
+			bufwrite = "msg",
+			confirm = "cmd",
+			emsg = "pager",
+			echo = "msg",
+			echomsg = "msg",
+			echoerr = "pager",
+			completion = "cmd",
+			list_cmd = "pager",
+			lua_error = "pager",
+			lua_print = "msg",
+			progress = "pager",
+			rpc_error = "pager",
+			quickfix = "msg",
+			search_cmd = "cmd",
+			search_count = "cmd",
+			shell_cmd = "pager",
+			shell_err = "pager",
+			shell_out = "pager",
+			shell_ret = "msg",
+			undo = "msg",
+			verbose = "pager",
+			wildlist = "cmd",
+			wmsg = "msg",
+			typed_cmd = "cmd",
+		},
+		cmd = {
+			height = 0.5,
+		},
+		dialog = {
+			height = 0.5,
+		},
+		msg = {
+			height = 0.3,
+			timeout = 5000,
+		},
+		pager = {
+			height = 0.5,
+		},
+	},
+})
